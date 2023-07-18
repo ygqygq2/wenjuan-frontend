8d0852841852f03d59458afaea899e08479ea183
ygqygq2 <ygqygq2@qq.com>
2023-07-18 13:03:19 +0800

    ci: 自定义域名使用 fqdn
    Co-authored-by: ygqygq2 <29ygq@sina.com>

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -1,6 +1,7 @@
 import { UserAddOutlined } from '@ant-design/icons';
 import { useRequest } from 'ahooks';
-import { Button, Checkbox, Form, Input, Space, Typography, message } from 'antd';
+import { Button, Form, Input, Space, Typography, message } from 'antd';
+import CryptoJS from 'crypto-js';
 import React, { FC, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 
@@ -17,26 +18,6 @@ const { Title } = Typography;
 const DEFAULT_USERNAME = 'test123';
 const DEFAULT_PASSWORD = '123456';
 
-const USERNAME_KEY = 'USERNAME';
-const PASSWORD_KEY = 'PASSWORD';
-
-function rememberUser(username: string, password: string) {
-  localStorage.setItem(USERNAME_KEY, username);
-  localStorage.setItem(PASSWORD_KEY, password);
-}
-
-function deleteUserFromStorage() {
-  localStorage.removeItem(USERNAME_KEY);
-  localStorage.removeItem(PASSWORD_KEY);
-}
-
-function getUserInfoFromStorage() {
-  return {
-    username: localStorage.getItem(USERNAME_KEY),
-    password: localStorage.getItem(PASSWORD_KEY),
-  };
-}
-
 const Login: FC = () => {
   const nav = useNavigate();
   const [form] = Form.useForm();
@@ -46,14 +27,11 @@ const Login: FC = () => {
     form.setFieldsValue({ username: DEFAULT_USERNAME, password: DEFAULT_PASSWORD });
   }, []);
 
-  useEffect(() => {
-    const { username, password } = getUserInfoFromStorage();
-    form.setFieldsValue({ username, password });
-  }, []);
-
   const { run } = useRequest(
     async (username: string, password: string) => {
-      const data = await loginService(username, password);
+      // 使用 CryptoJS 对密码进行加密
+      const encryptedPassword = CryptoJS.AES.encrypt(password, import.meta.env.CRYPTO_SECRET).toString();
+      const data = await loginService(username, encryptedPassword);
       return data;
     },
     {
@@ -70,16 +48,10 @@ const Login: FC = () => {
     },
   );
 
-  const onFinish = (values: any) => {
-    const { username, password, remember } = values || {};
+  const onFinish = async (values: any) => {
+    const { username, password } = values || {};
 
     run(username, password);
-
-    if (remember) {
-      rememberUser(username, password);
-    } else {
-      deleteUserFromStorage();
-    }
   };
 
   return (
@@ -114,9 +86,6 @@ const Login: FC = () => {
             </Form.Item>
             <Form.Item label="密码" name="password" rules={[{ required: true, message: '请输入密码' }]}>
               <Input.Password />
-            </Form.Item>
-            <Form.Item name="remember" valuePropName="checked" wrapperCol={{ offset: 6, span: 16 }}>
-              <Checkbox>记住我</Checkbox>
             </Form.Item>
             <Form.Item wrapperCol={{ offset: 6, span: 16 }}>
               <Space>
