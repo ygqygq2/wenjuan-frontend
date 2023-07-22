dd636449d79f45fc669a1a1117f8211ddab47c52
ygqygq2 <ygqygq2@qq.com>
2023-07-22 12:58:06 +0800

    refactor: 增加 salt 加密
    Co-authored-by: ygqygq2 <29ygq@sina.com>

--- src/utils/utils.ts
+++ src/utils/utils.ts
@@ -2,6 +2,6 @@ import CryptoJS from 'crypto-js';
 
 // 加密密码
 export const encryptPassword = (password: string): string => {
-  const hashedPassword = CryptoJS.SHA256(password).toString(CryptoJS.enc.Hex);
+  const hashedPassword = CryptoJS.SHA256(`${password}${import.meta.env.CRYPTO_SECRET}`).toString(CryptoJS.enc.Hex);
   return hashedPassword;
 };
